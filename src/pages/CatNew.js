9f7c7cb58339dcf6272b73508fdcd07113893f7e
Tricia Sykes <triciasykes@Tricias-MacBook-Pro-2.local>
2023-02-02 14:32:16 -0800

    added form and create functionality

--- src/pages/CatNew.js
+++ src/pages/CatNew.js
@@ -1,9 +1,93 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Form, FormGroup, Label, Input, Button } from 'reactstrap'
+import { useNavigate } from 'react-router-dom'
 
-const CatNew = () => {
+const CatNew = ({ createNewCat }) => {
 
+  const [newCat, setNewCat] = useState({
+    name: "",
+    age: "",
+    enjoys: "",
+    image: ""
+  })
+
+  const navigate = useNavigate()
+
+  const handleChange = (e) => {
+    //spread operator makes a copy of our state object (newCat)
+    // e.target.name will tell us which key we are updating based on the name property in input
+    // e.target.value will tell us what value we are setting to that key
+    setNewCat({...newCat, [e.target.name]: e.target.value})
+  }
+
+  const handleSubmit = () => {
+    createNewCat(newCat)
+    navigate("/catindex")
+  }
+ 
   return(
-    <> <h1>This is the CatNew</h1></>
+    <> 
+      <h1>Add a New Cat!</h1>  
+      <Form>
+        <FormGroup>
+          <Label for="name">
+            Name
+          </Label>
+          <Input
+            id="name"
+            name="name"
+            placeholder="with a placeholder"
+            type="text"
+            onChange={handleChange}
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label for="age">
+            Age
+          </Label>
+          <Input
+            id="age"
+            name="age"
+            placeholder="what's your age?"
+            type="text"
+            onChange={handleChange}
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label for="enjoys">
+            Enjoys
+          </Label>
+          <Input
+            id="enjoys"
+            name="enjoys"
+            placeholder="what do you like to do?"
+            type="text"
+            onChange={handleChange}
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label for="image">
+           Image 
+          </Label>
+          <Input
+            id="image"
+            name="image"
+            placeholder="please add image url"
+            type="text"
+            onChange={handleChange}
+          />
+        </FormGroup>
+      </Form>
+      <div>
+        <Button
+          color="primary"
+          name="submit"
+          onClick={handleSubmit}
+        >
+          Submit
+        </Button>
+      </div>
+    </>
   )
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,9 +3,9 @@ import React from 'react'
 const Home = () => {
 
   return(
-    <> 
-      <img src="https://images.unsplash.com/photo-1584125265217-8979882e8835?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTd8fGJsYWNrJTIwYW5kJTIwd2hpdGUlMjBjYXR8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60" class="rounded-circle" alt="Cute pic of Kevin" width="304" height="400"/>
-    </>
+    <div className="home-div">
+      <img src="https://images.unsplash.com/photo-1584125265217-8979882e8835?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTd8fGJsYWNrJTIwYW5kJTIwd2hpdGUlMjBjYXR8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60" className="rounded-circle" alt="Cute pic of Kevin" width="304" height="400"/>
+    </div>
   )
 }
 
