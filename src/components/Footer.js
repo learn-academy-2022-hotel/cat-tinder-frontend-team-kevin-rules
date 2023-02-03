e5ba7b548b6842e2b7190bb0e1333022876c726c
Charlean Baxter <baxtercm@outlook.com>
2023-02-02 21:03:29 -0800

    changed title, favicon, and footer styling

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,11 +4,15 @@ import year from '../assets/year.png'
 const Footer = () => {
 
   return(
-    <> 
-      <p> 
-        &copy; Presented by the Instruction Team | <img src={ year } height="12" alt="2023 in cat font" />        
-      </p>
-    </>
+    <div id="page-container">
+      <div id="content-wrap">
+        <footer> 
+          <p> 
+            &copy; Presented by the Instruction Team | <img src={ year } height="12" alt="2023 in cat font" />        
+          </p>
+        </footer>
+      </div>
+    </div>
   )
 }
 
